9282a05be7d402b7f996e65bcb444ff3c3970717
Vikrant singh <kingtvarshin1@gmail.com>
2019-08-07 14:19:54 +0530

    selection_left

--- js/jsonread.js
+++ js/jsonread.js
@@ -5,24 +5,31 @@ var obj = JSON.parse(teiDataValue);
 
 //lst
 var app = angular.module("myApp", []);
-app.controller("myCtrl", function($scope, $window) {
+app.controller("myCtrl", function($scope, $window) 
+{
   $scope.record0 = obj.firstArray;
   $scope.record1 = obj.secondArray;
   $scope.tranfer =[];
   $scope.tranfer2=[];
-
+  $scope.selectLeft = ""
+$scope.transer = $scope.selectLeft;
   //double click
-   $scope.sendr = function(index){
+   $scope.sendr = function(index)
+   {
     $scope.record1 = $scope.record1.concat($scope.record0[index])
     $scope.record0 = $scope.record0.filter( ( el ) => !$scope.record1.includes( el ) );
+    $scope.tranfer =[];
    };
-   $scope.sendl = function(index){
+   $scope.sendl = function(index)
+   {
     $scope.record0 = $scope.record0.concat($scope.record1[index])
     $scope.record1 = $scope.record1.filter( ( el ) => !$scope.record0.includes( el ) );
+    $scope.tranfer2=[];
    };
-
+   
   //onclick for left ul
-  $scope.selectri = function(index){
+  $scope.selectri = function(index)
+  {
     if($scope.tranfer.indexOf($scope.record0[index]) !== -1)
     {
       $scope.tranfer.splice($scope.tranfer.indexOf($scope.record0[index]),1)
@@ -34,7 +41,8 @@ app.controller("myCtrl", function($scope, $window) {
   };
 
     //onclick for right ul
-  $scope.selectlf = function(index){
+  $scope.selectlf = function(index)
+  {
     if($scope.tranfer2.indexOf($scope.record1[index]) !== -1)
     {
       $scope.tranfer2.splice($scope.tranfer2.indexOf($scope.record1[index]),1)
@@ -45,34 +53,34 @@ app.controller("myCtrl", function($scope, $window) {
   };
 
     //onclick for right btn
-   $scope.right = function(){
+   $scope.right = function()
+   {
     $scope.record1 = $scope.record1.concat($scope.tranfer)
+    $scope.tranfer2 = $scope.tranfer2.concat($scope.tranfer)
     $scope.record0 = $scope.record0.filter( ( el ) => !$scope.tranfer.includes( el ) );
     $scope.tranfer =[];
    };
 
    //onclick for left btn
-   $scope.left = function(){
+   $scope.left = function()
+   {
     $scope.record0 = $scope.record0.concat($scope.tranfer2)
+    $scope.tranfer = $scope.tranfer.concat($scope.tranfer2)
     $scope.record1 = $scope.record1.filter( ( el ) => !$scope.tranfer2.includes( el ) );
     $scope.tranfer2 =[];
    }; 
 
    //onclick for exchange
    $scope.both = function(){
-    if($scope.record0.length != 0){
+    if($scope.record0.length != 0)
+    {
       $scope.record1 = $scope.record1.concat($scope.record0);
       $scope.record0 = $scope.record0.filter( ( el ) => !$scope.record0.includes( el ) );
     }
-    else{
+    else
+    {
       $scope.record0 = $scope.record0.concat($scope.record1);
       $scope.record1 = $scope.record1.filter( ( el ) => !$scope.record1.includes( el ) );
     }
    };
-
-   
-  }
-
-
-
-  );
+});
